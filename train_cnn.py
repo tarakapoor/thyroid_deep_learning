e72ee629c68497be8752b85aa2af8bee495facc6
tarakapoor <44348827+tarakapoor@users.noreply.github.com>
2021-05-09 16:10:48 -0700

    Update train_cnn.py

--- train_cnn.py
+++ train_cnn.py
@@ -1,7 +1,7 @@
-from cnn_data_augmentations import * #augmentations are not functions so not sure what this is doing
+from cnn_data_augmentations import *
 import mobilenet_dataset
 import model_setup #has setup_model and save_networks functions
-import config #config.py
+from main import configwrite, configread
 import analyze_model_outputs
 
 import numpy as np
@@ -24,8 +24,9 @@ from sklearn.metrics import roc_auc_score, accuracy_score, roc_curve
 
 
 
+
 def roc_auc_score_FIXED(y_true, y_pred):
-    if (len(np.unique(y_true)) == 1): # only positives or only negatives in the sample; AUROC is not defined in this case
+    if (len(np.unique(y_true)) == 1): # bug in roc_auc_score
         return accuracy_score(y_true, np.rint(y_pred))
     return roc_auc_score(y_true, y_pred)
 
@@ -47,7 +48,8 @@ def focal_loss(bce_loss, targets, gamma, alpha):
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
-def train_model(args):
+def train_model(imgpath, maskpath, labelpath, project_home_dir):
+    config = configread()
     
     seed_value = 1
     torch.manual_seed(seed_value)
@@ -85,77 +87,79 @@ def train_model(args):
     epoch_aurocs = []
 
 
-    #default for early stopping; local variables
+    #default for early stopping
     min_val_loss = 10
     prev_val_loss = 10
     epochs_no_improve = 0
     early_stop = False
     min_epoch = 0
 
-    #max epochs to train for
-    if(config.trainval):
-        config.num_epochs = config.best_epoch+2
+    #max epochs
+    if(config['trainval'] == True):
+        config = configwrite('num_epochs', best_epoch+2)
     else:
-        config.num_epochs = 100
-    print("\n\nTraining CNN\ntraining for", config.num_epochs, "epochs")
+        config = configwrite('num_epochs', 100)
+    print("\n\nTraining CNN\ntraining for", config['num_epochs'], "epochs")
     
     #############DONE WITH DEFINING VARIABLES
     ###############NOW DEFINE MODEL
     
-    training_directory = "{0}".format(args.project_home_dir)
+    training_directory = "{0}".format(project_home_dir)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print("device", device)
     os.chdir(training_directory)
 
-    if(config.modeltype == 'mobilenet'):
+    if(config['modeltype'] == 'mobilenet'):
         model = models.mobilenet_v2(pretrained=True)
         for param in model.parameters():
             param.requires_grad = True
         model.classifier._modules['1'] = nn.Linear(1280, 256)
         model.classifier._modules['2'] = nn.Linear(256, 2)
 
-    print(config.modeltype, "model:")
+    print(config['modeltype'], "model:")
 
-    #FREEZE SOME LAYERS
+    #DO NOT FREEZE ANY LAYERS; CHANGE xfrozen IF YOU WANT TO FREEZE LAYERS
     ct = 0
     xfrozen = 0
     for child in model.children():
         for kid in child.children():
-            #freeze first xfrozen # of layers:
+            #freeze first xfrozen layers:
             if (ct < xfrozen):
                 for param in kid.parameters():
                     param.requires_grad = False
             ct += 1
     print('trainable parameters', count_parameters(model))
     
-    print("batch size: {}, learning rate: {}, output classes: {}, optimizer: SGD".format(config.batchSize, config.lr, config.num_classes))
+    print("batch size: {}, learning rate: {}, output classes: {}, optimizer: SGD".format(config['batchSize'], config['lr'], config['num_classes']))
 
     #moves to gpu
     model = model.to(device)
     
-    if(not config.trainval):
-        args.model_dir = config.modeltype + '_thyroid_weighted_focal_extralinear_adj_trainonly'
-        args.model_path = args.project_home_dir + 'model/' + args.model_dir + '/'
+    if(not config['trainval']):
+        model_dir = config['modeltype'] + '_thyroid_weighted_focal_extralinear_adj_trainonly'
+        model_path = project_home_dir + 'model/' + model_dir + '/'
     else: #trainval
-        args.model_dir = config.modeltype + '_thyroid_weighted_focal_extralinear_adj'
-        args.model_path = args.project_home_dir + 'model/' + args.model_dir + '/'
+        model_dir = config['modeltype'] + '_thyroid_weighted_focal_extralinear_adj'
+        model_path = project_home_dir + 'model/' + model_dir + '/'
+        
+    pretrained_dir = model_dir #only used by setup_model if in test phase
     
-    config.phase = "train"
-    model_setup.setup_model(model, "train", config.cvphase, args.model_dir, args.pretrained_dir, args)
-    print(config.modeltype, "Model Construction Complete")
+    config = configwrite('phase', "train")                           
+    model_setup.setup_model(model, "train", config['cvphase'], model_dir, pretrained_dir, project_home_dir, config['best_epoch'])
+    print(config['modeltype'], "Model Construction Complete")
 
-    #loss function
+
+    #adding weights to loss function because of imbalance in dataset
     criterion = nn.CrossEntropyLoss().to(device)
-    #optimizer
-    if(config.weightdecay):
-        optimizer = optim.SGD(model.parameters(), lr=config.lr, weight_decay = config.weightdecval, momentum=0.9)
+        
+    if(config['weightdecay']):
+        optimizer = optim.SGD(model.parameters(), lr=config['lr'], weight_decay = config['weightdecval'], momentum=0.9)
     else:
-        optimizer = optim.SGD(model.parameters(), lr=config.lr, momentum=0.9)
+        optimizer = optim.SGD(model.parameters(), lr=config['lr'], momentum=0.9)
 
-    #get weights for weighted data sampler
-    if(config.weighted_sampler):
+    if(config['weighted_sampler']):
         rowcount = 0
-        with open(args.project_home_dir + "samplesweight.csv", newline='') as infh:
+        with open(project_home_dir + "samplesweight.csv", newline='') as infh:
             print("opened csv for samplesweight")
             reader = csv.reader(infh)
             rowcount = 0
@@ -167,21 +171,23 @@ def train_model(args):
                     samples_weight.append(x)
             print(len(samples_weight))
     
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, len(samples_weight), eta_min=0.00001)    
+    
+    
     #load DATA
-    if(not config.trainval):
-        train_set = mobilenet_dataset.DatasetThyroid3StackedNew(args, "train", config.cvphase, config.frametype, transform=transformAug)
+    if(not config['trainval']):
+        train_set = mobilenet_dataset.DatasetThyroid3StackedNew(imgpath, maskpath, labelpath, project_home_dir, "train", config['cvphase'], config['frametype'], transform=transformAug)
         trainlen = len(train_set)
     else:
-        trainval_set = mobilenet_dataset.DatasetThyroid3StackedNew(args, "trainval", config.cvphase, config.frametype, transform=transformAug)
+        trainval_set = mobilenet_dataset.DatasetThyroid3StackedNew(imgpath, maskpath, labelpath, project_home_dir, "trainval", config['cvphase'], config['frametype'], transform=transformAug)
         trainlen = len(trainval_set)
-    val_set = mobilenet_dataset.DatasetThyroid3StackedNew(args, "val", config.cvphase, config.frametype, transform=transformNorm)
+    val_set = mobilenet_dataset.DatasetThyroid3StackedNew(imgpath, maskpath, labelpath, project_home_dir, "val", config['cvphase'], config['frametype'], transform=transformNorm)
             
-    #learning rate scheduler
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, len(samples_weight), eta_min=0.00001)    
-
+    
     ####START TRAINING!!!!
-    for epoch in range(config.num_epochs):
-        #OBTAINING LABELS and OUTPUTS BY EPOCH FOR AUROC
+    for epoch in range(config['num_epochs']):
+
+        #DOING THIS BY EPOCH FOR AUROC
         all_labels = []
         all_probs_ones = []
         all_patients = []
@@ -195,42 +201,42 @@ def train_model(args):
         valcount = 0.0
         traincount = 0.0
         
-        if(config.weighted_sampler):
+        if(config['weighted_sampler']):
             #weighted sampler:
             sampler = torch.utils.data.WeightedRandomSampler(samples_weight, len(samples_weight), replacement=True)
-            if(config.trainval):
-                train_set_loader = DataLoader(dataset=trainval_set, num_workers=0, batch_size=config.batchSize, sampler=sampler, shuffle=False)
+            if(config['trainval']):
+                train_set_loader = DataLoader(dataset=trainval_set, num_workers=0, batch_size=config['batchSize'], sampler=sampler, shuffle=False)
             else:
-                train_set_loader = DataLoader(dataset=train_set, num_workers=0, batch_size=config.batchSize, sampler=sampler, shuffle=False)
+                train_set_loader = DataLoader(dataset=train_set, num_workers=0, batch_size=config['batchSize'], sampler=sampler, shuffle=False)
 
         else:
             #no weighted sampler!
-            if(config.trainval):
-                train_set_loader = DataLoader(dataset=trainval_set, num_workers=0, batch_size=config.batchSize, shuffle=True)
+            if(config['trainval']):
+                train_set_loader = DataLoader(dataset=trainval_set, num_workers=0, batch_size=config['batchSize'], shuffle=True)
             else:
-                train_set_loader = DataLoader(dataset=train_set, num_workers=0, batch_size=config.batchSize, shuffle=True)
+                train_set_loader = DataLoader(dataset=train_set, num_workers=0, batch_size=config['batchSize'], shuffle=True)
         
-        val_set_loader = DataLoader(dataset=val_set, num_workers=0, batch_size=config.batchSize, shuffle=False)
+        val_set_loader = DataLoader(dataset=val_set, num_workers=0, batch_size=config['batchSize'], shuffle=False)
         
         if(epoch%10 == 0):
-            if(not config.trainval):
+            if(not config['trainval']):
                 print('Loading total', trainlen, 'training images--------')
             else:
                 print('Loading total', trainlen, 'training images--------')
             print('Loading total', len(val_set), 'val images--------')
 
+        
         #UPDATE LEARNING RATE ONCE PER EPOCH
         if(epoch>0 and (epoch%10 == 0)):
             scheduler.step()
-            print("Learning rate at epoch", epoch, "is:", optimizer.param_groups[0]['lr'])
+            print("Learning rate at epoch", epoch, "is:", optimizer.param_groups[0]['lr'])#, "aka", scheduler.get_last_lr())
         else:
-            print("Learning rate at epoch", epoch, "is:", config.lr, "aka", optimizer.param_groups[0]['lr'])
+            print("Learning rate at epoch", epoch, "is:", config['lr'], "aka", optimizer.param_groups[0]['lr'])
         lrs.append(optimizer.param_groups[0]['lr'])
         
-        
+        #this calls getitem (for each i in train_set_loader)
         print("num iterations of train:", len(train_set_loader))
         model.train() #train mode
-        #this calls getitem (for each i in train_set_loader)
         for i, data in enumerate(train_set_loader):
             inputs = data['input'].to(device)
             labels = data['label'].to(device)
@@ -249,7 +255,7 @@ def train_model(args):
 
             try:
                 loss = criterion(outputs, labels)
-                f_loss = focal_loss(loss, labels, config.floss_gamma, config.floss_alpha)
+                f_loss = focal_loss(loss, labels, config['floss_gamma'], config['floss_alpha'])
             except:
                 print("1train loss failed", inputs.shape, outputs.shape, labels.shape)
             try:
@@ -264,7 +270,7 @@ def train_model(args):
                 print("2train loss failed", inputs.shape, outputs.shape, labels.shape)
 
             #TRAIN ACCURACY
-            if (config.probFunc == 'Softmax'):
+            if (config['probFunc'] == 'Softmax'):
                 sf = nn.Softmax(dim=1) #makes items in a row add to 1; dim = 0 makes items in a column add to 1
                 outputs = sf(outputs)
 
@@ -298,20 +304,18 @@ def train_model(args):
                     for j, dataa in enumerate(val_set_loader):
                         tinputs = dataa['input'].to(device)
                         tlabels = dataa['label'].to(device)
-                        #new
                         tannot_ids = dataa['annot_id']
-                        tframe_nums = dataa['frame_num']
 
 
                         # Forward pass only to get logits/output
                         outs = model(tinputs)
-                        tlabels = tlabels.squeeze(1)#tlabels.squeeze()
+                        tlabels = tlabels.squeeze(1)
 
                         # Get predictions from the maximum value                        
-                        if (config.probFunc == 'Softmax'):
+                        if (config['probFunc'] == 'Softmax'):
                             sf = nn.Softmax(dim=1) #makes items in a row add to 1; dim = 0 makes items in a column add to 1
                             outs = sf(outs)
-                        elif (config.probFunc == 'Sigmoid'):
+                        elif (config['probFunc'] == 'Sigmoid'):
                             sf = nn.Sigmoid()
                             outs = sf(outs)
 
@@ -337,7 +341,7 @@ def train_model(args):
                             try:
                                 #val loss
                                 loss_val = criterion(outs, tlabels)
-                                f_loss_val = focal_loss(loss_val, tlabels, config.floss_gamma, config.floss_alpha)
+                                f_loss_val = focal_loss(loss_val, tlabels, config['floss_gamma'], config['floss_alpha'])
 
                                 running_loss_val += loss_val.item()
                                 running_focal_loss_val += f_loss_val.item()
@@ -350,7 +354,7 @@ def train_model(args):
                         # Total correct predictions
                         try:
                             if torch.cuda.is_available():
-                                correct += (predicted.cpu() == tlabels.cpu()).sum()#(thresh_predicted == tlabels.cpu()).sum()
+                                correct += (predicted.cpu() == tlabels.cpu()).sum()
                             else:
                                 correct += (predicted == tlabels).sum()
                         except:
@@ -366,6 +370,7 @@ def train_model(args):
                         accuracy = 100 * correct // total
                         #end of torch no grad
 
+
                 #print statistics
                 message = 'epoch: %d, iters: %d, time: %.3f, with loss: %.3f, focal weighted loss: %.5f, val loss: %.3f, val focal weighted loss: %.5f, train_acc: %.4f, val accuracy: %.3f' % (
                     epoch, i + 1, time.time() - epochstart, loss, f_loss, loss_val, f_loss_val, trainaccuracy.data, accuracy.data)
@@ -378,8 +383,8 @@ def train_model(args):
         total_all += total
         correct_all += correct
 
-        losses.append(running_loss / traincount)#len(train_set))
-        f_losses.append(running_focal_loss / traincount)#len(train_set))
+        losses.append(running_loss / traincount)
+        f_losses.append(running_focal_loss / traincount)
 
         val_loss = running_loss_val / valcount
 
@@ -387,7 +392,7 @@ def train_model(args):
         f_losses_val.append(running_focal_loss_val / valcount)
         print("Epoch", epoch, "overall val loss:", val_loss, "overall train loss:", (running_loss / traincount), "or train over len trainset?", (running_loss / trainlen))
 
-        if(not config.trainval):
+        if(not config['trainval']):
             if ((val_loss < prev_val_loss) and epoch != 0): #current epoch val loss improved
           # Save the model
                 epochs_no_improve = 0
@@ -401,7 +406,7 @@ def train_model(args):
                 epochs_no_improve += 1
                 prev_val_loss = val_loss
 
-            if (epoch > 40 and epochs_no_improve >= config.n_epochs_stop):
+            if (epoch > 40 and epochs_no_improve >= config['n_epochs_stop']):
                 print('Early stopping! end of epoch', epoch, ", val loss =", val_loss, "min val loss =", min_val_loss, "from epoch", min_epoch)
                 early_stop = True
 
@@ -458,17 +463,17 @@ def train_model(args):
             break
 
         save_freq = 10
-        if ((epoch % save_freq == 0) or (config.trainval and epoch >= (config.best_epoch-2))):
-            print('saving the latest model (epoch %d) of learning rate %f' % (epoch, config.lr))
+        if ((epoch % save_freq == 0) or (config['trainval'] and epoch >= (config['best_epoch-2']))):
+            print('saving the latest model (epoch %d) of learning rate %f' % (epoch, config['lr']))
             
-            if(not config.trainval):
-                args.model_dir = config.modeltype + '_thyroid_weighted_focal_extralinear_adj_trainonly'
-                args.model_path = args.project_home_dir + 'model/' + args.model_dir + '/'
+            if(not config['trainval']):
+                model_dir = config['modeltype'] + '_thyroid_weighted_focal_extralinear_adj_trainonly'
+                model_path = project_home_dir + 'model/' + model_dir + '/'
             else: #trainval
-                args.model_dir = config.modeltype + '_thyroid_weighted_focal_extralinear_adj'
-                args.model_path = args.project_home_dir + 'model/' + args.model_dir + '/'
-            print("to:", args.model_path)
-            model_setup.save_networks(model, epoch, config.cvphase, args.model_path, args.model_dir)
+                model_dir = config['modeltype'] + '_thyroid_weighted_focal_extralinear_adj'
+                model_path = project_home_dir + 'model/' + model_dir + '/'
+            print("to:", model_path)
+            model_setup.save_networks(model, epoch, config['cvphase'], model_path, model_dir)
     
     try:
         full_auroc = roc_auc_score(all_labels, all_probs_ones)
@@ -512,16 +517,10 @@ def train_model(args):
     patientwise_auroc = roc_auc_score(patientlabels, patient_ave_preds)
     print("\n\nTotal AUROC by patient-wise average predictions:", patientwise_auroc)
     
-    if(not config.trainval):
+    if(not config['trainval']):
         print("Min validation loss epoch:", min_epoch, ", loss =", min_val_loss, ", train loss =", losses[min_epoch], ", val auroc =", epoch_aurocs[min_epoch])
     else:
-        print("Min train 3/5 validation loss epoch:", config.best_epoch, ", loss =", min_val_loss)
-    
-    #plt.plot(lrs)
-    #print(lrs)
-    #plt.xlabel("Epochs")
-    #plt.ylabel("Learning rates")
-    #plt.show()
+        print("Min train 3/5 validation loss epoch:", config['best_epoch'], ", loss =", min_val_loss)
     
     analyze_model_outputs.plot_test_stats(losses, f_losses, losses_val, f_losses_val, epoch_aurocs, patientlabels, patient_ave_preds)
     analyze_model_outputs.calc_test_stats(patients, patientlabels, patient_ave_preds)
